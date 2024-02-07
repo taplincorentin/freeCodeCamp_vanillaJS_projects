cfb4f73b6f5404b0cbd4c0d7cc7997d22f55a854
Corentin <taplincorentin@gmail.com>
2024-02-07 21:34:20 +0100

    did js loop and conditions

---
+++ course02_counter/app.js
@@ -0,0 +1,22 @@
+//set initial count
+let count = 0;
+
+//get value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('reset')){
+            count=0;
+        }
+        else{
+            count++;
+        }
+        value.textContent = count;
+    })
+});
